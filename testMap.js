0ddd5394d4ccd46772135923a267798903937596
Mahesh <96672896+smahesh21@users.noreply.github.com>
2022-10-10 18:10:15 +0530

    Update testMap.js
    first commit

--- testMap.js
+++ testMap.js
@@ -3,7 +3,7 @@ let mapFunction = require('./map')
 const items = [1, 2, 3, 4, 5, 5];
 
 function addEachItemUsingMap(number){
-   return number
+   return number * number
 }
 
 const newArray = mapFunction(items,addEachItemUsingMap)
