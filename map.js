a806eb9c0ec2c6c8ffe3f0d14cdde6082ff67b3a
Mahesh <96672896+smahesh21@users.noreply.github.com>
2022-10-18 10:56:05 +0530

    Code is modified in this file.

--- map.js
+++ map.js
@@ -1,9 +1,13 @@
-let map = (items, addEachItemFunction) => {
-    let newArray = []
-    for (let num of items){
-        let addedItemToNewArray = addEachItemFunction(num)
-        newArray.push(addedItemToNewArray)
+const map = (items, addEachItemFunction) => {
+    if (items.length !== 0) {
+        const newArray = []
+        for (let num of items){
+            const addedItemToNewArray = addEachItemFunction(num)
+            newArray.push(addedItemToNewArray)
+        }
+        return newArray
+    } else {
+        return `The array you passed as an argument is empty`
     }
-    return newArray
 }
 module.exports = map
