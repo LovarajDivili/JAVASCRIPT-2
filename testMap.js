150b3d8770ba9e2dd09f57bd2ee8a30bacbd3594
Mahesh <96672896+smahesh21@users.noreply.github.com>
2022-10-18 11:01:01 +0530

    Code is modified in this file.

--- testMap.js
+++ testMap.js
@@ -1,11 +1,10 @@
-let mapFunction = require('./map')
+const mapFunction = require('./map')
 
 const items = [1, 2, 3, 4, 5, 5];
 
-function addEachItemUsingMap(number){
-   return number * number
-}
 
-const newArray = mapFunction(items,addEachItemUsingMap)
+const addEachItemUsingMap = (number) => number * number
 
-console.log(newArray)
+const resultOfMap = mapFunction(items,addEachItemUsingMap)
+
+console.log(resultOfMap)
