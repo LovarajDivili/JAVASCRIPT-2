60d79b5c094c2b836331c27b28c455494d0cc6ef
Mahesh <96672896+smahesh21@users.noreply.github.com>
2022-10-18 10:59:27 +0530

    Code is modified in this file.

--- testFind.js
+++ testFind.js
@@ -1,9 +1,8 @@
-let find = require('./find')
+const find = require('./find')
 const items = [1,2,3,4,5,5]
-function findElementGreaterOrEqualToFour(number) {
-    return number >= 4
-}
 
-const result = find(items,findElementGreaterOrEqualToFour)
+const findElementGreaterOrEqualToGivenNumber = (number,numberToSearchInTheItems = 5) => number >= numberToSearchInTheItems
 
-console.log(result)
+const resultOfFind = find(items,findElementGreaterOrEqualToGivenNumber)
+
+console.log(resultOfFind)
