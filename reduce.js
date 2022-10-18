c53663d17b35aa3a1a525fb8ec88223ad1376c21
Mahesh <96672896+smahesh21@users.noreply.github.com>
2022-10-18 10:56:50 +0530

    Code is modified in this file.

--- reduce.js
+++ reduce.js
@@ -1,17 +1,25 @@
-let reduce = (items,reducerFunction,startingValue) => {
-    if (startingValue === 0) { 
-        startingValue = items[0]
+const reduce = (items,reducerFunction,startingValue) => {
+    if (items.length !== 0) {
+        let numberAfterSum = 0
+        if (items.length === 1) {
+            return items[0]
+        } else if (startingValue === undefined) {
+            startingValue = items[0]
+            for (let i = 1;i<items.length;i++) {
+                numberAfterSum = reducerFunction(startingValue,items[i])
+                startingValue = numberAfterSum
+            }
+            return numberAfterSum
+        } else if (items.length > 1) {
+            for (let i = 0;i<items.length;i++) {
+                numberAfterSum = reducerFunction(startingValue,items[i])
+                startingValue = numberAfterSum
+            }
+            return numberAfterSum
+        }
+    } else {
+        return "The array you passed as argument is empty"
     }
-    let numberAfterSum = 0
-    for (let i = 1;i<items.length;i++) {
-        num = items[i]
-        //console.log(num)
-        numberAfterSum = reducerFunction(startingValue,num)
-        startingValue = numberAfterSum
-    }
-    return numberAfterSum
-    
-
 }
 
 module.exports = reduce
