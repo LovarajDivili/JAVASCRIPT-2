3e44a4a4fd77a14e0ef89e80cf8c14467c4773f7
Mahesh <96672896+smahesh21@users.noreply.github.com>
2022-10-18 10:57:33 +0530

    Code is modified in this file.

--- testEach.js
+++ testEach.js
@@ -1,12 +1,13 @@
-let each = require('./each')
+const each = require('./each')
 
 const items = [1, 2, 3, 4, 5, 5];
-let newArray = []
 
-function addEachItem(number,index){
-    let indexWithNum = (`Index-${index},${number}`)
-    newArray.push(indexWithNum)
+const newItemsWithIndex = []
+
+const addEachItem = (number,index) => {
+    newItemsWithIndex.push(`Index-${index},${number}`)
+    return newItemsWithIndex
 }
 
-each(items,addEachItem)
-console.log(newArray)
+const resultOfEach = each(items,addEachItem)
+console.log(resultOfEach)
